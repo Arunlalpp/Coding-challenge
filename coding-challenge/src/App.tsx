43885dff2d9fe3a6b38b73fba75156b1f425d357
Arunlal PP <arunlalpp@gmail.com>
2023-07-18 23:02:13 +0530

    feat: birthday remider

--- coding-challenge/src/App.tsx
+++ coding-challenge/src/App.tsx
@@ -1,13 +1,17 @@
-import defaultSellerData from "./data";
-import SellerApp from "./components/SellerApp";
-import BinarySearch from "./components/BinarySearch";
+// import defaultSellerData from "./data";
+// import SellerApp from "./components/SellerApp";
+// import BinarySearch from "./components/BinarySearch";
+
+import { VonnueBirthday } from "./components/VonnueBirthday";
 
 function App() {
   return (
     <div className="bg-slate-600 flex flex-col justify-center items-center h-screen w-screen">
-      <SellerApp sellerData={defaultSellerData} />
+      {/* <SellerApp sellerData={defaultSellerData} /> */}
       <div className="py-8">
-        <BinarySearch />
+        {/* <BinarySearch /> */}
+
+        <VonnueBirthday UTCTimes="2022-07-29T06:04:32Z" />
       </div>
     </div>
   );
---
+++ coding-challenge/src/birthdayData.ts
@@ -0,0 +1,20 @@
+// eslint-disable-next-line import/no-anonymous-default-export
+
+// eslint-disable-next-line import/no-anonymous-default-export
+export default {
+  weekDays: ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"],
+  month: [
+    "Jan",
+    "Feb",
+    "Mar",
+    "Apr",
+    "May",
+    "Jun",
+    "Jul",
+    "Aug",
+    "Sep",
+    "Oct",
+    "Nov",
+    "Dec",
+  ],
+};
