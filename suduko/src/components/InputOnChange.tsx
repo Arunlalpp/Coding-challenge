ad5c8a7a1f4b3bf838356015f164bc09c9a9966e
Arunlal PP <arunlalpp@gmail.com>
2023-08-06 20:09:20 +0530

    feat: client x nad y points with 3 buttons

--- suduko/src/components/InputOnChange.tsx
+++ suduko/src/components/InputOnChange.tsx
@@ -7,31 +7,86 @@ export interface TPoint {
 
 function InputOnChange() {
   const [points, setPoints] = useState<TPoint[]>([]);
+  const [popped, setPopped] = useState<TPoint[]>([]);
+
   const handleOnChange = (e: React.MouseEvent<HTMLDivElement>) => {
-    const { pageX, pageY } = e;
+    const { clientX, clientY } = e;
 
     setPoints([
       ...points,
       {
-        x: pageX,
-        y: pageY,
+        x: clientX,
+        y: clientY,
       },
     ]);
     console.log(points);
   };
 
+  const handleUndo = () => {
+    const newPoints = [...points];
+    const poppedPoints = newPoints.pop();
+    if (!poppedPoints) {
+      return;
+    }
+    setPopped([...popped, poppedPoints]);
+    setPoints(newPoints);
+  };
+
+  const handleRedo = () => {
+    const newPopped = [...popped];
+    const newPoints = [...popped];
+
+    const poppedPoints = popped.pop();
+    if (!poppedPoints) {
+      return;
+    }
+
+    points.push(poppedPoints);
+    setPoints([...newPoints]);
+    setPopped([...newPopped]);
+  };
+
+  const handleClear = () => {
+    setPoints([]);
+  };
+
   return (
-    <div
-      className="bg-red-300 relative h-screen w-screen"
-      onClick={handleOnChange}
-      role="button"
-    >
-      {points.map((point) => (
-        <div
-          className="absolute inline-block rounded-full h-3.5 w-3.5 bg-blue-900"
-          style={{ left: point.x + "px", top: point.y + "px" }}
-        ></div>
-      ))}
+    <div>
+      <div className="flex justify-center items-center gap-4">
+        <button
+          onClick={handleUndo}
+          type="button"
+          className="py-2 px-4 bg-violet-800 text-white rounded-lg"
+        >
+          Undo
+        </button>
+        <button
+          onClick={handleRedo}
+          type="button"
+          className="py-2 px-4 bg-violet-800 text-white rounded-lg"
+        >
+          Redo
+        </button>
+        <button
+          onClick={handleClear}
+          type="button"
+          className="py-2 px-4 bg-violet-800 text-white rounded-lg"
+        >
+          Clear
+        </button>
+      </div>
+      <div
+        className="bg-red-300 relative h-screen w-screen flex justify-center items-center"
+        onClick={handleOnChange}
+        role="button"
+      >
+        {points.map((point) => (
+          <div
+            className="absolute inline-block rounded-full h-3.5 w-3.5 bg-blue-900"
+            style={{ left: point.x - 5 + "px", top: point.y - 5 + "px" }}
+          ></div>
+        ))}
+      </div>
     </div>
   );
 }
