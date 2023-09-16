b967202787705a8c242b1b1fb85333118c5c8d68
Arunlal PP <arunlalpp@gmail.com>
2023-09-17 00:06:08 +0530

    feat: puzzle

--- ConvergentPuzzle/Convergent.js
+++ ConvergentPuzzle/Convergent.js
@@ -6,8 +6,6 @@ function threeNPlusOne(number) {
   return 3 * number + 1;
 }
 
-console.log(threeNPlusOne(10));
-
 function convergentSeriesLength(maxLengthNumber) {
   const initialValue = 1;
   for (let i = maxLengthNumber; i != 1; i = threeNPlusOne(i)) {
@@ -16,8 +14,6 @@ function convergentSeriesLength(maxLengthNumber) {
 
   return initialValue;
 }
-
-console.log(convergentSeriesLength(10));
 
 function convergentSeriesMaxLength(value1, value2) {
   let maxLength = 0;
